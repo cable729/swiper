a9f4de44b908efcace58f10d6f56b2e7d557791e
Caleb Jares <cable729@gmail.com>
2014-03-12 14:03:32 -0500

    compiled and successfully running

---
+++ app/src/main/java/com/calebjares/swiper/ui/MainActivity.java
@@ -0,0 +1,9 @@
+package com.calebjares.swiper.ui;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class MainActivity extends SherlockFragmentActivity {
+}
