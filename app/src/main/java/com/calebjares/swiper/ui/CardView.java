e1ddc86a4b67a6c49cc88e3e8cb4dee47af1a133
Caleb Jares <cable729@gmail.com>
2014-03-12 16:48:13 -0500

    the card can now be SWIPED :shipit:

--- app/src/main/java/com/calebjares/swiper/ui/CardView.java
+++ app/src/main/java/com/calebjares/swiper/ui/CardView.java
@@ -1,10 +1,9 @@
 package com.calebjares.swiper.ui;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.calebjares.swiper.R;
@@ -14,8 +13,13 @@ import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
 
 @EViewGroup(R.layout.view_card)
-public class CardView extends LinearLayout {
+public class CardView extends RelativeLayout {
     @ViewById(R.id.card_text) TextView text;
+    private float lastX;
+    private float lastY;
+    private int originalLeftMargin;
+    private int originalTopMargin;
+    private boolean hasSetOriginalMargins;
 
     public CardView(Context context) {
         super(context);
@@ -25,12 +29,49 @@ public class CardView extends LinearLayout {
         super(context, attrs);
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public CardView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
     public void setCard(Card card) {
         text.setText(card.text);
+    }
+
+    @Override public boolean onTouchEvent(MotionEvent event) {
+        RelativeLayout.LayoutParams params = (LayoutParams) this.getLayoutParams();
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                lastX = event.getRawX();
+                lastY = event.getRawY();
+                if (!hasSetOriginalMargins) {
+                    originalLeftMargin = params.leftMargin;
+                    originalTopMargin = params.topMargin;
+                    hasSetOriginalMargins = true;
+                }
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                float dx = event.getRawX() - lastX;
+                float dy = event.getRawY() - lastY;
+                params.leftMargin += dx;
+                params.topMargin += dy;
+                lastX = event.getRawX();
+                lastY = event.getRawY();
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                params.leftMargin = originalLeftMargin;
+                params.topMargin = originalTopMargin;
+                break;
+            }
+            case MotionEvent.ACTION_CANCEL: {
+                params.leftMargin = originalLeftMargin;
+                params.topMargin = originalTopMargin;
+                break;
+            }
+        }
+        this.setLayoutParams(params);
+        return true;
     }
 }
