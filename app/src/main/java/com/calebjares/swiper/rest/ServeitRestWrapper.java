af1c205bea0c6ed5f4c580091e37eb87ba1e49b7
Caleb Jares <cable729@gmail.com>
2014-03-18 23:27:27 -0500

    added all current card methods

--- app/src/main/java/com/calebjares/swiper/rest/ServeitRestWrapper.java
+++ app/src/main/java/com/calebjares/swiper/rest/ServeitRestWrapper.java
@@ -1,9 +1,19 @@
 package com.calebjares.swiper.rest;
 
+import java.util.List;
+
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface ServeitRestWrapper {
+    @POST("cards.json")
+    ApiCard createCard(@Query("text") String text, @Query("latitude") float latitude, @Query("longitude") float longitude);
     @GET("cards/new.json")
     ApiCard newCard(@Query("text") String text, @Query("latitude") float latitude, @Query("longitude") float longitude);
+    @GET("cards.json")
+    List<ApiCard> getCards(@Query("limit") int limit);
+    @GET("cards/{id}.json")
+    ApiCard getCard(@Path("id") int id);
 }
